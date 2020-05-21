ee23c7531311fa14ecfec230508d44be4f5504e3
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-21 11:31:02 -0300

    Add factory_bot support

--- backend/spec/support/database_cleaner.rb
+++ backend/spec/support/database_cleaner.rb
@@ -1,10 +1,10 @@
 RSpec.configure do |config|
     config.before(:suite) do
-      DatabaseCleaner.strategy = :truncation
+        DatabaseCleaner.strategy = :truncation
     end
-  
+
     config.before(:each) do
-      DatabaseCleaner.clean
+        DatabaseCleaner.clean
     end
-  end
+end
   

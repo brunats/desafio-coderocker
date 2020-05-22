18972884bb0fe9630c76dbc76c94b5bccb2c6b2d
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-22 12:37:23 -0300

    Att seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,7 @@
 20.times  do
     Post.create({
-        title: Faker::Book.title,
-        body: Faker::Lorem.sentence,
-        image_url: Faker::Internet.url
+      title: Faker::Book.title,
+      body: Faker::Lorem.sentence,
+      image_url: Faker::Internet.url
     })
 end

3e8e04189a3d187949a45cf3dbbdbaf358651972
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-22 15:11:29 -0300

    Add controller and route to provide access to one post

--- backend/app/controllers/api/v1/posts_controller.rb
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -3,7 +3,16 @@ module API
         class PostsController < ApplicationController
             def index
               posts = Post.order('created_at DESC');
-              render json: {status: 'SUCCESS', message: 'Posts carregados', data: posts}, status: :ok
+              render json: {status: :ok, message: 'Posts carregados', data: posts}, status: :ok
+            end
+
+            def show
+              begin
+                post = Post.find(params[:id])
+                render json: {status: :ok, message:'Loaded post', data: post}, status: :ok
+              rescue => exception
+                render json: {status: :bad_request, message:'Post not found', data: exception},status: :bad_request
+              end
             end
         end
     end

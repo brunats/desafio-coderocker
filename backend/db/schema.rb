4619d9fc3263857821967976328165a21bcb376f
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-22 22:01:55 -0300

    Add migration of Contact

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_174201) do
+ActiveRecord::Schema.define(version: 2020_05_23_004746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.string "post"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"

d6f72dca1d5aa64cc83d7935ab38b714b17f41e0
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-23 20:35:20 -0300

    Add Material-UI library

---
+++ frontend/src/pages/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component} from 'react'
+
+import { Button } from '@material-ui/core'
+
+class Layout extends Component {
+  render() {
+    return (
+      <>
+        <h1>Começo da nossa aplicação React 2!</h1>
+        <Button color="primary">Hello World</Button>
+      </>
+    )
+  }
+}
+
+export default Layout;

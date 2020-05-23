70ace1b1c22fad27bd00ed81c53c6bdcb0713359
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-23 13:02:31 -0300

    Add contact controller - create action

--- backend/app/controllers/api/v1/contacts_controller.rb
+++ backend/app/controllers/api/v1/contacts_controller.rb
@@ -5,6 +5,21 @@ module API
           contacts = Contact.all
           render json: { data: contacts }, status: :ok
         end
+
+        def create
+          contact = Contact.new(contact_params)
+          if contact.save
+            render json: { created: true, errors: {} }, status: :created 
+          else
+            render json: { created: false, errors: contact.errors }, status: :bad_request
+          end
+        end
+
+        private
+        
+        def contact_params
+          params[:contact].permit(:name, :email, :phone, :post)
+        end
       end
   end
 end
--- backend/spec/requests/api/v1/contacts_controller_spec.rb
+++ backend/spec/requests/api/v1/contacts_controller_spec.rb
@@ -19,4 +19,65 @@ RSpec.describe API::V1::ContactsController, type: :request do
       end
     end
   end
+
+  describe 'Contacts #create' do
+    context 'when pass valid contact' do
+      let(:contact) { build(:contact) }
+      
+      before(:each) do
+        contact
+        post api_v1_contacts_path, params: {
+          contact: {
+            name: contact.name,
+            email: contact.email,
+            phone: contact.phone,
+            post: contact.post
+          }
+        }
+      end
+
+      it 'return http created' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'return created true' do
+        expect(json_response[:created]).to eq(true)
+      end
+
+      it 'returns erros empty ' do
+        expect(json_response[:errors]).to be_empty
+      end
+    end
+
+    context 'when pass invalid contact without post attribute' do
+      let(:contact) { build(:contact) }
+      
+      before(:each) do
+        contact
+        post api_v1_contacts_path, params: {
+          contact: {
+            name: contact.name,
+            email: contact.email,
+            phone: contact.phone
+          }
+        }
+      end
+
+      it 'returns http bad_request' do
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'return created false' do
+        expect(json_response[:created]).to eq(false)
+      end
+
+      it 'returns erros not empty' do
+        expect(json_response[:errors]).not_to be_empty
+      end
+
+      it 'return post attribute error cannot be null' do
+        expect(json_response[:errors][:post]).to eq(['can\'t be blank'])
+      end
+    end
+  end
 end

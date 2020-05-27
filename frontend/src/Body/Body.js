18db1cc1b3a409970738740b76a71887b362d62a
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-27 11:03:15 -0300

    Change header to stay fixed in scroll

--- frontend/src/Body/Body.js
+++ frontend/src/Body/Body.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+
+import { createMuiTheme } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
@@ -21,7 +24,18 @@ function Header() {
   const classes = useStyles();
 
   return (
-    <p>Olá</p>
+    <Container>
+        <Box my={2}>
+          {[...new Array(50)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
+        </Box>
+      </Container>
   );
 }
 
--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+
 
 const theme = createMuiTheme({
   palette: {
@@ -30,21 +34,48 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-function Header() {
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0
+  })
+
+  if(trigger) {
+    return React.cloneElement(children, {
+      elevation: 4,
+      positionFixed: true,
+    })
+  }
+
+  return React.cloneElement(children, {
+    elevation: 0,
+    position: "static"
+  })
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired
+};
+
+function Header(props) {
   const classes = useStyles();
 
   return (
     <div>
       <MuiThemeProvider theme={theme}>
-        <AppBar position="static" color="primary">
-          <Toolbar className={classes.toolbar}>
-            <Typography variant="h5" className={classes.title}>
-              Rockr Blog
-            </Typography>
-            <Button color="inherit">Posts</Button>
-            <Button color="inherit">Contact</Button>
-          </Toolbar>
-        </AppBar>
+        <ElevationScroll {...props}>
+          <AppBar color="primary">
+            <Toolbar className={classes.toolbar}>
+              <Typography variant="h5" className={classes.title}>
+                Rockr Blog
+              </Typography>
+              <Button color="inherit">Posts</Button>
+              <Button color="inherit">Contact</Button>
+            </Toolbar>
+          </AppBar>
+        </ElevationScroll>
       </MuiThemeProvider>
     </div>
   );

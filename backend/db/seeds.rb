c7a44ba1357e57a0e778b82c5a5ab8ba52400694
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-28 10:21:01 -0300

    Add author in table posts

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,8 @@
-20.times  do
+2.times  do
     Post.create({
+      author: Faker::Name.name_with_middle,
       title: Faker::Book.title,
-      body: Faker::Lorem.sentence,
-      image_url: Faker::Internet.url
+      body: Faker::Lorem.paragraph_by_chars(number: 1200),
+      image_url: "https://source.unsplash.com/640x640/?Rock-concert"
     })
 end

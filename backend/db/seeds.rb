977f3e188804926d6f966da4097257d8fd62c910
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-28 15:10:41 -0300

    Improved seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,8 +1,34 @@
-2.times  do
-    Post.create({
-      author: Faker::Name.name_with_middle,
-      title: Faker::Book.title,
-      body: Faker::Lorem.paragraph_by_chars(number: 1200),
-      image_url: "https://source.unsplash.com/640x640/?Rock-concert"
-    })
-end
+Post.create({
+  author: Faker::Name.name_with_middle,
+  title: Faker::Book.title,
+  body: Faker::Lorem.paragraph_by_chars(number: 1200),
+  image_url: "https://images.unsplash.com/photo-1547044398-55241c261234?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=640&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=640"
+})
+
+Post.create({
+  author: Faker::Name.name_with_middle,
+  title: Faker::Book.title,
+  body: Faker::Lorem.paragraph_by_chars(number: 1200),
+  image_url: "https://images.unsplash.com/photo-1561357875-04c4292fcc96?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=640&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=640"
+})
+
+Post.create({
+  author: Faker::Name.name_with_middle,
+  title: Faker::Book.title,
+  body: Faker::Lorem.paragraph_by_chars(number: 1200),
+  image_url: "https://images.unsplash.com/photo-1541190990694-4a612732721c?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=640&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=640"
+})
+
+Post.create({
+  author: Faker::Name.name_with_middle,
+  title: Faker::Book.title,
+  body: Faker::Lorem.paragraph_by_chars(number: 1200),
+  image_url: "https://images.unsplash.com/photo-1566981731417-d4c8e17a9e82?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=640&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=640"
+})
+
+Post.create({
+  author: Faker::Name.name_with_middle,
+  title: Faker::Book.title,
+  body: Faker::Lorem.paragraph_by_chars(number: 1200),
+  image_url: "https://images.unsplash.com/photo-1580151755089-075031f569b7?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=640&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=640"
+})

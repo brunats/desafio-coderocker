f44cb0418dceb1257951ae6775e15b1ff90c3385
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-28 14:42:43 -0300

    Add mount in getAll

--- frontend/src/Body/PostsProcessing/GetAll.js
+++ frontend/src/Body/PostsProcessing/GetAll.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react'
 
 import SmallCard from '../../Cards/SmallCard'
+import BigCard from '../../Cards/BigCard'
 import Classified from './Classified'
+import Mount from './Mount'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  line: {
+    marginTop: '30px',
+  },
+})
 
 const GetAllPosts = "http://127.0.0.1:4000/api/v1/posts/"
 
@@ -9,6 +19,8 @@ function GetAll() {
   const [error, setError] = useState(null)
   const [isLoaded, setIsLoaded] = useState(false)
   const [items, setItems] = useState([])
+
+  const style = useStyles()
 
   useEffect(() => {
     fetch(GetAllPosts)
@@ -26,15 +38,25 @@ function GetAll() {
   }, [])
 
   if (error) {
+
     return <div>Error: {error.message}</div>
+
   } else if (!isLoaded) {
+
     return <div>Loading...</div>
+
+  } else if (items.length > 0) {
+
+      const classifiedItems = Classified(items)
+      const posts = Mount(classifiedItems).map((post) =>
+        <div className={style.line}>{post}</div>
+      )
+
+      console.log('posts', posts)
+
+      return posts
   } else {
-    const classifiedItems = Classified(items)
-    
-    return (
-      <SmallCard posts={items}/>
-    )
+    return <></>
   }
 }
 

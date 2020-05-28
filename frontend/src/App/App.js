c52274ef7d4ee6633bf9fb1f64598381485e9e32
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-27 21:27:28 -0300

    Change style to component

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React from 'react'
 
-import Header from '../Header/Header';
-import Body from '../Body/Body';
+import Header from '../Header/Header'
+import Body from '../Body/Body'
 
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100%',
+    width: '100%',
+    left: '0%',
+    right: '0%',
+    top: '0%',
+    bottom: '0%',
+
+    textAlign: 'center',
+    
+    background: 'linear-gradient(90deg, #F1A10A 0%, #342303 100%)',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+  },
+  
+}))
 
 function App() {
+  const style = useStyles()
+
   return (
-    <div className="App">
+    <div className={style.root}>
       <Header />
       <Body />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

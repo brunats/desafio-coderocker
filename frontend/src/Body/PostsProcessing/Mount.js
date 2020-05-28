6d26bbc60140c492719686ba50a8246a1d69ca44
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-28 14:42:03 -0300

    Create function to mount list of posts following the classifier

---
+++ frontend/src/Body/PostsProcessing/Mount.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import SmallCard from '../../Cards/SmallCard'
+import BigCard from '../../Cards/BigCard'
+
+function Mount(items) {
+  const postPlace = ['big','smallRight', 'smallLeft']
+  let mountPosts = []
+
+  let smallPost = []
+
+  items.map((item, index) => {
+    const key = item[0]
+    const post = item[1]
+
+    if (key == 'big') {
+      mountPosts.push(<BigCard post={post} />)
+    }
+    else {
+      if (key == 'small') {
+        mountPosts.push(<SmallCard posts={post} />)
+      }
+    }
+  })
+
+  return mountPosts
+}
+
+export default Mount

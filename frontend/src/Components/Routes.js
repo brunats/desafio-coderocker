bfa242bc252884b0c2c287fcc1aa487b730fb843
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-28 21:18:22 -0300

    Delete route of contact page

--- frontend/src/Components/Routes.js
+++ frontend/src/Components/Routes.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Home from '../Pages/Home'
-import Contact from '../Pages/Contact'
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/contact" component={Contact} />
     </Switch>
 </ BrowserRouter>
 )
